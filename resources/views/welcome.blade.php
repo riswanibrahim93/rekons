be3575996873b79303e7fcffd4c183576cc401cd
Kacong Arip <arifsaqa@gmail.com>
2021-10-28 17:21:41 +0700

    hmm

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.main')
 @section('title', 'Home')
 @section('content')
-    fff
+<div class="row">
+    <div class="col-sm-12">
+        <div class="card">
+            <div class="card-header">
+                <h5>Example</h5>
+            </div>
+            <div class="card-body">
+               
+            </div>
+        </div>
+    </div>
+
+</div>
+@endsection
+@section('script')
+{{-- <script>
+    $('#aha').on('click',function () {
+            myLoader('#aha','show');
+            setTimeout(() => {
+                myLoader('#aha','hide');
+            }, 2000);
+        })
+</script> --}}
 @endsection
