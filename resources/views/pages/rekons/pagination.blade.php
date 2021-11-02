f4c72957ed0aa104d35cceb130115bcef0ac3fb2
Kacong Arip <arifsaqa@gmail.com>
2021-11-02 09:29:18 +0700

    ss

--- resources/views/pages/rekons/pagination.blade.php
+++ resources/views/pages/rekons/pagination.blade.php
@@ -12,14 +12,24 @@
           <td>{{$item->data->plafond??''}}</td>
           <td>{{$item->data->atr??''}}</td>
           <td>{{$item->data->outstanding??''}}</td>
-          <td class="font-secondary">
-            @if ($item->status==1)
-            No Atribusi
+          <td>
+            <span class="@if ($item->status==0)
+            badge badge-danger
+            @elseif($item->status==1)
+            badge badge-warning
             @elseif($item->status==2)
             Beda Fasilitas
             @else
             Valid
-            @endif
+            @endif">
+              @if ($item->status==1)
+              No Atribusi
+              @elseif($item->status==2)
+              Beda Fasilitas
+              @else
+              Valid
+              @endif
+            </span>
           </td>
         </tr>
         @empty
